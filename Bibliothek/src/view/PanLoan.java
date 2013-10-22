094849ec04bb9c064d6c3525b9d80e720c0b0f64
Nicola <Nicola@152.96.236.156>
2013-10-22 11:36:02 +0200

    Fertig für däääääää morge

--- Bibliothek/src/view/PanLoan.java
+++ Bibliothek/src/view/PanLoan.java
@@ -1,16 +1,30 @@
 package view;
 
 import javax.swing.JPanel;
+
 import java.awt.BorderLayout;
 import java.awt.GridBagLayout;
+
 import javax.swing.JLabel;
+
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
+
 import javax.swing.border.TitledBorder;
 import javax.swing.border.LineBorder;
+import javax.swing.table.AbstractTableModel;
+
 import java.awt.Color;
 
+import javax.swing.JTextField;
+import javax.swing.JCheckBox;
+import javax.swing.JButton;
+import javax.swing.JTable;
+import javax.swing.JScrollPane;
+
 public class PanLoan extends JPanel {
+	private JTextField textField;
+	private JTable table;
 
 	/**
 	 * Create the panel.
@@ -72,7 +86,75 @@ public class PanLoan extends JPanel {
 		JPanel panel_1 = new JPanel();
 		panel_1.setBorder(new TitledBorder(new LineBorder(new Color(0, 0, 0)), "Loans", TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		add(panel_1, BorderLayout.CENTER);
+		panel_1.setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setBorder(new TitledBorder(null, "All Loans are in the table below", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panel_1.add(panel_2, BorderLayout.NORTH);
+		GridBagLayout gbl_panel_2 = new GridBagLayout();
+		gbl_panel_2.columnWidths = new int[]{0, 0, 86, 0, 0};
+		gbl_panel_2.rowHeights = new int[]{20, 0};
+		gbl_panel_2.columnWeights = new double[]{1.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_panel_2.rowWeights = new double[]{0.0, Double.MIN_VALUE};
+		panel_2.setLayout(gbl_panel_2);
+		
+		textField = new JTextField();
+		GridBagConstraints gbc_textField = new GridBagConstraints();
+		gbc_textField.insets = new Insets(0, 0, 0, 5);
+		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField.gridx = 0;
+		gbc_textField.gridy = 0;
+		panel_2.add(textField, gbc_textField);
+		textField.setColumns(10);
+		
+		JCheckBox chckbxNewCheckBox = new JCheckBox("Show only overdue loans");
+		GridBagConstraints gbc_chckbxNewCheckBox = new GridBagConstraints();
+		gbc_chckbxNewCheckBox.insets = new Insets(0, 0, 0, 5);
+		gbc_chckbxNewCheckBox.gridx = 1;
+		gbc_chckbxNewCheckBox.gridy = 0;
+		panel_2.add(chckbxNewCheckBox, gbc_chckbxNewCheckBox);
+		
+		JButton btnNewButton = new JButton("New button");
+		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
+		gbc_btnNewButton.insets = new Insets(0, 0, 0, 5);
+		gbc_btnNewButton.gridx = 2;
+		gbc_btnNewButton.gridy = 0;
+		panel_2.add(btnNewButton, gbc_btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("New button");
+		GridBagConstraints gbc_btnNewButton_1 = new GridBagConstraints();
+		gbc_btnNewButton_1.gridx = 3;
+		gbc_btnNewButton_1.gridy = 0;
+		panel_2.add(btnNewButton_1, gbc_btnNewButton_1);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		panel_1.add(scrollPane, BorderLayout.CENTER);
+		
+		table = new JTable();
+		table.setModel(new AbstractTableModel(){
+			private String[] columnNames = new String[] {
+					"Availalbe", "Title", "Author", "Publisher"
+			};
+
+			@Override
+			public int getColumnCount() {
+				// TODO Auto-generated method stub
+				return 0;
+			}
+
+			@Override
+			public int getRowCount() {
+				// TODO Auto-generated method stub
+				return 0;
+			}
+
+			@Override
+			public Object getValueAt(int arg0, int arg1) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+		});	
+	scrollPane.setViewportView(table);
 
 	}
-
 }
